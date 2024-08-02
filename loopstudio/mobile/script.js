a5835ba9cdfd56cb8038ebd4a5fcc7a3e075ea74
Lahari <140933241+lahari7chandu@users.noreply.github.com>
2024-08-02 17:54:35 +0530

    Add files via upload

---
+++ loopstudio/mobile/script.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+
+  hamburger.addEventListener("click", function () {
+    hamburger.classList.toggle("is-active");
+    menu.classList.toggle("is-active");
+  });
+};
